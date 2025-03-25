34a0d3b85d7b2028df1b2c52e5d778df9e150ad5
sonai-2005 <nandisonai60@gmail.com>
2025-03-25 23:24:03 +0530

    add the descriptions

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -10,7 +10,7 @@ export default function Card() {
             <div className="px-5 pb-5">
                 <a href="/">
                     <h5 className="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">
-                        Apple Watch Series 7 GPS, Aluminium Case, Starlight Sport
+                       Saptarshi Nandi (2nd Year ECE AOT)
                     </h5>
                 </a>
                 <div className="flex items-center mt-2.5 mb-5">
@@ -64,7 +64,7 @@ export default function Card() {
                     </span>
                 </div>
                 <div className="flex items-center justify-between">
-                    <span className="text-3xl font-bold text-gray-900 dark:text-white">$599</span>
+                    <span className="text-3xl font-bold text-gray-900 dark:text-white">$1</span>
                     <a
                         href="/"
                         className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
